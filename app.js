69c682f3d9af12105afb2dcfb4099e4e3e687abc
kg782 <keigo@designroyale.com.au>
2013-10-24 18:25:59 +1100

    Store access token in session with db

--- app.js
+++ app.js
@@ -9,8 +9,40 @@ var http = require('http');
 var path = require('path');
 var configs = require('./configs');
 var MongoStore = require('connect-mongo')(express);
+var passport = require('passport');
+var TwitterStrategy = require('passport-twitter').Strategy;
 
 var app = express();
+
+// Passport session setup.
+//   To support persistent login sessions, Passport needs to be able to
+//   serialize users into and deserialize users out of the session.  Typically,
+//   this will be as simple as storing the user ID when serializing, and finding
+//   the user by ID when deserializing.  However, since this example does not
+//   have a database of user records, the complete Twitter profile is serialized
+//   and deserialized.
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+// Passport settings
+passport.use(new TwitterStrategy({
+    consumerKey: configs.TWITTER_CONSUMER_KEY,
+    consumerSecret: configs.TWITTER_CONSUMER_SECRET,
+    callbackURL: "http://127.0.0.1:9000/twitter/callback"
+  },
+  function(token, tokenSecret, profile, done) {
+    profile.token = token;
+    profile.tokenSecret = tokenSecret;
+    process.nextTick(function () {
+      return done(null, profile);
+    });
+  }
+));
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -22,6 +54,8 @@ app.use(express.session({
     secret: configs.COOKIE_SECRET,
     store: new MongoStore(configs.MONGO_DB)
 }));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(app.router);
 //app.use(express.static(path.join(__dirname, 'public')));
 
@@ -30,9 +64,35 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-// API
-app.all('/twitter/rest/*', twitter.rest);
+// Twitter login
+app.get('/twitter/login', passport.authenticate('twitter'));
+app.get('/twitter/callback',
+  passport.authenticate('twitter', {failureRedirect: '/'}),
+  function(req, res) {
+    res.redirect('/');
+  }
+);
+app.get('/twitter/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+app.get('/twitter/error-auth', twitter.errorAuth);
+
+// Twitter API
+app.all('/twitter/rest/*', ensureAuthenticated, twitter.rest);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+// Simple route middleware to ensure user is authenticated.
+//   Use this route middleware on any resource that needs to be protected.  If
+//   the request is authenticated (typically via a persistent login session),
+//   the request will proceed.  Otherwise, the user will be redirected to the
+//   login page.
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/twitter/error-auth');
+}
--- routes/twitter.js
+++ routes/twitter.js
@@ -1,22 +1,31 @@
 var configs = require('../configs.js');
-var twitter = require('ntwitter');
-
-var twit = new twitter({
-  consumer_key: configs.TWITTER_CONSUMER_KEY,
-  consumer_secret: configs.TWITTER_CONSUMER_SECRET,
-  access_token_key: configs.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: configs.TWITTER_ACCESS_TOKEN_SECRET
-});
+var Twitter = require('ntwitter');
 
 exports.rest = function(req, res) {
-    var paths = req.path.split('/');
-    if (paths.length >= 4 && twit[paths[3]]) {
-        twit[paths[3]](function (err, data) {
-          res.send(data);
-        });
-    } else {
-        res.send({
-            error: 'Error: mothod not found'
-        });
-    }
+
+  console.log(req.session);
+
+  var twit = new Twitter({
+    consumer_key: configs.TWITTER_CONSUMER_KEY,
+    consumer_secret: configs.TWITTER_CONSUMER_SECRET,
+    access_token_key: req.session.passport.user.token,
+    access_token_secret: req.session.passport.user.tokenSecret
+  });
+
+  var paths = req.path.split('/');
+  if (paths.length >= 4 && twit[paths[3]]) {
+    twit[paths[3]](function (err, data) {
+      res.send(data);
+    });
+  } else {
+    res.send({
+      error: 'Error: mothod not found'
+    });
+  }
+};
+
+exports.errorAuth = function(req, res) {
+  res.send({
+    error: "No authenticated"
+  });
 };
