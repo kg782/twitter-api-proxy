3259a50e03dbaf57cd16832432e5af73e561fe0a
kg782 <keigo@designroyale.com.au>
2013-10-29 15:35:17 +1100

    Cleaned up

--- app.js
+++ app.js
@@ -15,13 +15,6 @@ var mongoose = require('mongoose');
 
 var app = express();
 
-// Passport session setup.
-//   To support persistent login sessions, Passport needs to be able to
-//   serialize users into and deserialize users out of the session.  Typically,
-//   this will be as simple as storing the user ID when serializing, and finding
-//   the user by ID when deserializing.  However, since this example does not
-//   have a database of user records, the complete Twitter profile is serialized
-//   and deserialized.
 passport.serializeUser(function(user, done) {
   done(null, user.id);
 });
